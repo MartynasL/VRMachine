426ba4c1d3198c0f98597bbb8509bd3123699929
inteloexo <inteloexo@gmail.com>
2014-03-07 19:46:37 +0200

    biski klase ir shit

---
+++ VirtualRealMachine/MuchClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualRealMachine
+{
+    class MuchClass
+    {
+        public void main(String[] args)
+        {
+            System.Console.Write("labas");
+            Console.Beep(55, 5555);
+        }
+    }
+}
