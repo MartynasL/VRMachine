0d3172a06b846fb223f0645f48fc33f4500bacd1
Martynas <martynas.lycius@gmail.com>
2014-03-08 15:09:14 +0200

    Parašyta Word klasė, kuri atitinka atminties žodį. Taip pat parašyta bazinė VirtualMemory klasė atitinka paprastą darbą su 100 žodžių virtualia atmintim.

---
+++ VirtualRealMachine/VirtualMemory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualRealMachine
+{
+    public class VirtualMemory
+    {
+        Word[] memoryWords = new Word[100];
+
+        public VirtualMemory()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                memoryWords[i] = new Word("0000");
+            }
+        }
+
+        public Word getWordAtAddress(int address)
+        {
+            try
+            {
+                return memoryWords[address];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                throw new IndexOutOfRangeException("There is no such address in memory");
+            }            
+        }
+
+        public void setWordAtAddress(int address, Word word)
+        {
+            try
+            {
+                memoryWords[address] = word;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                throw new IndexOutOfRangeException("There is no such address in memory");
+            }   
+        }
+    }
+}
---
+++ VirtualRealMachine/Word.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualRealMachine
+{
+    class Word
+    {
+        private char[] word = new char[4];
+
+        public Word(string stringWord)
+        {
+            setWord(stringWord);
+        }
+
+        public void setWord(string stringWord)
+        {
+            if (stringWord.Length == 4)
+            {
+                word = stringWord.ToCharArray();
+            }
+            else
+            {
+                throw new FormatException("Valid word cannot be created");
+            }
+        }
+
+        //byte number from 1 to 4. 1st is eldest byte.
+        public char getWordByte(int byteNumber)
+        {
+            return word[byteNumber];
+        }
+
+    }
+}
