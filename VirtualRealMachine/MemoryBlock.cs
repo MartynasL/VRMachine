59f577209ca52e96192530ad565d7803c8a88456
Martynas <martynas.lycius@gmail.com>
2014-03-09 13:25:13 +0200

    Papildyta MemoryBlock klasė get ir set metodais.

--- VirtualRealMachine/MemoryBlock.cs
+++ VirtualRealMachine/MemoryBlock.cs
@@ -9,6 +9,39 @@ namespace VirtualRealMachine
     class MemoryBlock
     {
         private Word[] words;
+        private const int WORDS_IN_BLOCK = 10;
 
+        public MemoryBlock()
+        {
+            words = new Word[WORDS_IN_BLOCK];
+            for (int i = 0; i < WORDS_IN_BLOCK; i++)
+            {
+                words[i] = new Word("0000");
+            }
+        }
+
+        public Word getBlockWord(int position)
+        {
+            try
+            {
+                return words[position];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                throw new IndexOutOfRangeException("There is no such position in block");
+            }        
+        }
+
+        public void setBlockWord(int position, Word word)
+        {
+            try
+            {
+                words[position] = word;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                throw new IndexOutOfRangeException("There is no such position in block");
+            }   
+        }
     }
 }
