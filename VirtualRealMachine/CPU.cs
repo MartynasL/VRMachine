983ed7c871a155c825a9bcd8bc348399a2074339
inteloexo <inteloexo@gmail.com>
2014-03-10 18:35:19 +0200

    prideti tusti metodai i CPU

--- VirtualRealMachine/CPU.cs
+++ VirtualRealMachine/CPU.cs
@@ -25,5 +25,140 @@ namespace VirtualRealMachine
         private Register K2 = new Register1B();
         private Register K3 = new Register1B();
         private Register C = new Register1B();
+
+        private void addRegisterMemory()
+        {
+
+        }
+
+        private void addRegisters()
+        {
+
+        }
+
+        private void subRegisterMemory()
+        {
+
+        }
+
+        private void subRegisters()
+        {
+
+        }
+
+        private void mulRegisterMemory()
+        {
+
+        }
+
+        private void mulRegisters()
+        {
+
+        }
+
+        private void divRegisterMemory()
+        {
+
+        }
+
+        private void divRegisters()
+        {
+
+        }
+
+        private void incRegister()
+        {
+
+        }
+
+        private void decRegister()
+        {
+
+        }
+
+        private void loadRegister()
+        {
+
+        }
+
+        private void saveRegister()
+        {
+
+        }
+
+        private void copyRegister()
+        {
+
+        }
+
+        private void compRegisterMemory()
+        {
+
+        }
+
+        private void compRegisters()
+        {
+
+        }
+
+        private void jump()
+        {
+
+        }
+
+        private void input()
+        {
+
+        }
+
+        private void output()
+        {
+
+        }
+
+        private void push()
+        {
+
+        }
+
+        private void pop()
+        {
+
+        }
+
+        private void getRegister()
+        {
+
+        }
+
+        private void setRegister()
+        {
+
+        }
+
+        private void slave()
+        {
+
+        }
+
+        private void halt()
+        {
+
+        }
+
+        private void changeMode()
+        {
+
+        }
+
+        private void exchange()
+        {
+
+        }
+
+        private void test()
+        {
+
+        }
     }
 }
